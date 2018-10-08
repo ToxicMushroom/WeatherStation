e4f5b697429043b945acdd6a29812f2a7942e966
ToxicMushroom <merlijn.verstraete@gmail.com>
2018-10-08 10:00:36 +0200

    Vooruitgang

--- flaskr/database.py
+++ flaskr/database.py
@@ -3,7 +3,6 @@ import threading
 import random
 import sqlite3
 
-stats_list = {"temperaturen": []}
 
 def current_milli_time():
     return int(round(time.time() * 1000))
@@ -12,27 +11,23 @@ def current_milli_time():
 def init():
     conn = sqlite3.connect('stats.db')
     c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS temps (bigint time, int degree)''')
+    c.execute("CREATE TABLE IF NOT EXISTS temps (time bigint, degree int)")
     conn.commit()
     conn.close()
     threading.Thread(target=dummy_data, args=()).start()
 
 
 def dummy_data():
-
     while 1:
         add_temp(random.randint(-10, 30))
         time.sleep(5)
 
 
 def add_temp(temp):
-
+    time = current_milli_time()
     # Keep list at length 5 History will be added latest
-    if len(stats_list["temperaturen"]) > 4:
-        stats_list["temperaturen"][0] = stats_list["temperaturen"][1]
-        stats_list["temperaturen"][1] = stats_list["temperaturen"][2]
-        stats_list["temperaturen"][2] = stats_list["temperaturen"][3]
-        stats_list["temperaturen"][3] = stats_list["temperaturen"][4]
-        stats_list["temperaturen"].pop()
-
-    stats_list["temperaturen"].append({'time': current_milli_time(), 'temp': temp})
+    conn = sqlite3.connect('stats.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO temps (time, degree) VALUES ('" + str(time) + "', '" + str(temp) + "' )")
+    conn.commit()
+    conn.close()
--- flaskr/server.py
+++ flaskr/server.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, jsonify
+import math
+import sqlite3
+
+from flask import Flask, render_template, jsonify, request
 import database
 
 app = Flask(__name__)
@@ -6,14 +9,37 @@ app = Flask(__name__)
 database.init()
 
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def index():
     return render_template('index.html')
 
 
-@app.route("/api/")
+@app.route("/api/temperatuur", methods=['GET'])
 def api():
-    return jsonify(database.stats_list)
+    size = request.args.get('size')
+    minuten = request.args.get('minuten')
+
+    if size is None:
+        size = 5
+    else:
+        size = int(request.args.get('size'))
+
+    if minuten is None:
+        minuten = 1
+    else:
+        minuten = int(request.args.get('minuten'))
+
+    lijst = []
+
+    conn = sqlite3.connect('stats.db')
+    c = conn.cursor()
+    c.execute("SELECT time, degree FROM temps WHERE time > " + str(database.current_milli_time() - minuten * 60 * 1000))
+    for row in c.fetchall():
+        lijst.append({'time': row[0], 'degree': row[1]})
+    conn.close()
+
+    math.ceil(len(lijst)/size)
+    return jsonify(lijst)
 
 
 if __name__ == '__main__':
