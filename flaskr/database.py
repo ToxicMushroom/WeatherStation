93024a344bfe1b44d872aba6f97f4a8d4d2dd62b
ToxicMushroom <merlijn.verstraete@gmail.com>
2018-11-22 14:23:57 +0100

    Added new part

--- flaskr/database.py
+++ flaskr/database.py
@@ -1,12 +1,13 @@
 import time
 import threading
-import random
+from Adafruit_BME280 import *
 import sqlite3
 import serial
+import random
 
 ser = serial.Serial("/dev/ttyUSB0", 9600)
 ser.flushInput()
-
+sensor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)
 
 def current_milli_time():
     return int(round(time.time() * 1000))
@@ -17,16 +18,16 @@ def init():
     c = conn.cursor()
     c.execute("CREATE TABLE IF NOT EXISTS temps (time bigint, degree int)")
     c.execute("CREATE TABLE IF NOT EXISTS speeds (time bigint, speed double)")
+    c.execute("CREATE TABLE IF NOT EXISTS presures (time bigint, speed double)")
     conn.commit()
     conn.close()
     threading.Thread(target=reader, args=()).start()
 
 
 def reader():
-    while 1:
-        linein = ser.readline()
-        if linein != "":
-            print(linein)
+    add_temp(sensor.read_temperature())
+    add_presure(sensor.read_pressure()/100)
+    add_speed(random.randint(0, 40))
 
 
 def add_temp(temp):
@@ -35,6 +36,16 @@ def add_temp(temp):
     conn = sqlite3.connect('stats.db')
     c = conn.cursor()
     c.execute("INSERT INTO temps (time, degree) VALUES ('" + str(time) + "', '" + str(temp) + "' )")
+    conn.commit()
+    conn.close()
+
+
+def add_presure(presure):
+    time = current_milli_time()
+    # Keep list at length 5 History will be added latestx
+    conn = sqlite3.connect('stats.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO presures (time, presure) VALUES ('" + str(time) + "', '" + str(presure) + "' )")
     conn.commit()
     conn.close()
 
--- flaskr/server.py
+++ flaskr/server.py
@@ -2,7 +2,6 @@ import math
 import sqlite3
 import re
 
-
 from flask import Flask, render_template, jsonify, request
 import database
 
@@ -55,6 +54,44 @@ def api_temp():
     return jsonify(lijst)
 
 
+@app.route("/api/druk", methods=['GET'])
+def api_presure():
+    size = request.args.get('size')
+    minuten = request.args.get('minuten')
+
+    if not pattern.match(str(size)):
+        size = 5
+    else:
+        size = int(request.args.get('size'))
+
+    if not pattern.match(str(minuten)):
+        minuten = 1
+    else:
+        minuten = int(request.args.get('minuten'))
+
+    volledige_lijst = []
+
+    conn = sqlite3.connect('stats.db')
+    c = conn.cursor()
+    c.execute("SELECT time, presure FROM presures WHERE time > " + str(database.current_milli_time() - minuten * 60 * 1000))
+    for row in c.fetchall():
+        volledige_lijst.append({'time': row[0], 'presure': row[1]})
+    conn.close()
+
+    lijst = []
+    last_i = 0
+    for i in range(0, len(volledige_lijst), math.ceil(len(volledige_lijst) / size)):
+        gemmidelde = volledige_lijst[i]
+        if i != 0:
+            for b in range(last_i, i):
+                gemmidelde['presure'] = gemmidelde['presure'] + volledige_lijst[b]['presure']
+            gemmidelde['presure'] = gemmidelde['presure'] / math.ceil(len(volledige_lijst) / size)
+        lijst.append(gemmidelde)
+        last_i = i
+
+    return jsonify(lijst)
+
+
 @app.route("/api/windsnelheid", methods=['GET'])
 def api_speed():
     size = request.args.get('size')
@@ -96,4 +133,3 @@ def api_speed():
 
 if __name__ == '__main__':
     app.run(debug=True, port=80, host='0.0.0.0')
-
